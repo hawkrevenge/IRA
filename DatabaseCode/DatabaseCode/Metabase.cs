3b765fc436c942eaad0d3a7e4e7f43db0ec836ce
hawkrevenge <roelvw@live.nl>
2016-05-22 21:42:24 +0200

    manier om alles op te vragen voor de QF gestard

--- DatabaseCode/DatabaseCode/Metabase.cs
+++ DatabaseCode/DatabaseCode/Metabase.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SQLite;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace DatabaseCode
 {
@@ -14,20 +15,22 @@ namespace DatabaseCode
         static String[] tables = { "mpg", "cylinders", "displacement", "horsepower", "weight", "acceleration", "model_year", "origin", "brand", "model", "type" };
 
         SQLiteConnection m_mbConnection;
+        SQLiteConnection m_dbConnection;
 
-        public Metabase(SQLiteConnection connection)
+        public Metabase(SQLiteConnection connection, SQLiteConnection dbconnenction)
         {
             instance = this;
             m_mbConnection = connection;
+            m_dbConnection = dbconnenction;
         }
 
-        private void ExecuteCommand(String s)
+        private void ExecuteCommand(String s, SQLiteConnection connection)
         {
-            SQLiteCommand command = new SQLiteCommand(s, m_mbConnection);
+            SQLiteCommand command = new SQLiteCommand(s, connection);
             SQLiteDataReader reader = command.ExecuteReader();
             while (reader.Read())
             {
-                Console.WriteLine(reader.GetInt32(0));
+                Console.WriteLine(reader.GetFloat(0));
                 //Console.WriteLine(reader.GetString(0));
             }
         }
@@ -35,13 +38,28 @@ namespace DatabaseCode
         public void InsertAll()
         {
             InsertQF();
+
         }
 
         public void InsertQF()
         {
+            Dictionary<string,Dictionary<string, int>> Dictionary = new Dictionary<string, Dictionary<string, int>>();
+            StreamReader reader = new StreamReader("workload.txt");
+            reader.ReadLine(); reader.ReadLine();
+            string line;
+            string[] splitted;
+            do
+            {
+                line = reader.ReadLine();
+                splitted = Regex.Split(line, " times: ");
+                ExecuteCommand(splitted[1], m_dbConnection);
+                //SQLiteCommand command = new SQLiteCommand(line, );
+            }
+            while (line != "");
             //ExecuteCommand("SELECT name FROM sqlite_temp_master WHERE type=\'table\'");
             //ExecuteCommand("SELECT name FROM sqlite_master \nWHERE type IN('table', 'view') AND name NOT LIKE 'sqlite_%'\nUNION ALL\nSELECT name FROM sqlite_temp_master\nWHERE type IN('table', 'view')\nORDER BY 1");
-            ExecuteCommand("SELECT * FROM mpg");
+            ExecuteCommand("SELECT * FROM autompg WHERE brand = 'saab' ", m_dbConnection);
+            Console.WriteLine("executed: QF-Values");
         }
     }
 }
