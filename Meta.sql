57d3313515c1d661d169681796102de18b7432ae
hawkrevenge <roelvw@live.nl>
2016-05-25 17:23:09 +0200

    metaChange

--- Meta.sql
+++ Meta.sql
@@ -1,14 +1,14 @@
-CREATE TABLE mpg (id integer NOT NULL, QF real, IDF real, PRIMARY KEY (id) );
-CREATE TABLE cylinders (id integer NOT NULL, QF real, IDF real, PRIMARY KEY (id) );
-CREATE TABLE displacement (id integer NOT NULL, QF real, IDF real, PRIMARY KEY (id) );
-CREATE TABLE horsepower (id integer NOT NULL, QF real, IDF real, PRIMARY KEY (id) );
-CREATE TABLE weight (id integer NOT NULL, QF real, IDF real, PRIMARY KEY (id) );
-CREATE TABLE acceleration (id integer NOT NULL, QF real, IDF real, PRIMARY KEY (id) );
-CREATE TABLE model_year (id integer NOT NULL, QF real, IDF real, PRIMARY KEY (id) );
-CREATE TABLE origin (id integer NOT NULL, QF real, IDF real, PRIMARY KEY (id) );
-CREATE TABLE brand (id integer NOT NULL, QF real, IDF real, PRIMARY KEY (id) );
-CREATE TABLE model (id integer NOT NULL, QF real, IDF real, PRIMARY KEY (id) );
-CREATE TABLE type (id integer NOT NULL, QF real, IDF real, PRIMARY KEY (id) );
+CREATE TABLE mpg (id real NOT NULL, QF real, IDF real, PRIMARY KEY (id) );
+CREATE TABLE cylinders (id real NOT NULL, QF real, IDF real, PRIMARY KEY (id) );
+CREATE TABLE displacement (id real NOT NULL, QF real, IDF real, PRIMARY KEY (id) );
+CREATE TABLE horsepower (id real NOT NULL, QF real, IDF real, PRIMARY KEY (id) );
+CREATE TABLE weight (id real NOT NULL, QF real, IDF real, PRIMARY KEY (id) );
+CREATE TABLE acceleration (id real NOT NULL, QF real, IDF real, PRIMARY KEY (id) );
+CREATE TABLE model_year (id real NOT NULL, QF real, IDF real, PRIMARY KEY (id) );
+CREATE TABLE origin (id real NOT NULL, QF real, IDF real, PRIMARY KEY (id) );
+CREATE TABLE brand (id string NOT NULL, QF real, IDF real, PRIMARY KEY (id) );
+CREATE TABLE model (id string NOT NULL, QF real, IDF real, PRIMARY KEY (id) );
+CREATE TABLE type (id string NOT NULL, QF real, IDF real, PRIMARY KEY (id) );
 
 
 
