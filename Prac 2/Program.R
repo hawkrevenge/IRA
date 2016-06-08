ee2431a1a1d548fee81262836615cc69da3a49ee
hawkrevenge <roelvw@live.nl>
2016-06-08 16:04:04 +0200

    ...

--- Prac 2/Program.R
+++ Prac 2/Program.R
@@ -1,4 +1,15 @@
+library("NLP")
 library("tau")
 library("tm")
 library("MASS")
 library("nnet")
+
+Main<- function(){
+  #start het programma
+  
+}
+
+stringprint <-function(x){
+  x
+}
+stringprint("test")
