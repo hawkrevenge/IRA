a42c34242386838e0584affaa6e7bc9f0662d102
hawkrevenge <roelvw@live.nl>
2016-06-09 08:59:31 +0200

    test

--- Prac 2/Program.R
+++ Prac 2/Program.R
@@ -6,10 +6,48 @@ library("nnet")
 
 Main<- function(){
   #start het programma
-  
+  #head(description)
+  if(checkFunc())
+    ReadInfunc()
+
+}
+equallength<-function(x,y)
+{
+  x==y
 }
 
-stringprint <-function(x){
-  x
+all.queryterms <- function (queries,docs)
+{
+  a<-sapply(queries, method="string",n=1L, textcnt)
+  b<-sapply(sapply(docs, method="string",n=1L, textcnt), names)
+  c<-mapply(intersect,sapply(a,names),b)
+  feature<- sapply((mapply(equallength,sapply(a,length),sapply(c,length))), as.numeric)
+  #n <- length(queries)
+  #feature <- vector(length=n)
+  #for(i in 1:n){
+  #  query <- queries[i]
+  #  document <- docs[i]
+  #  a <- textcnt(query,method="string",n=1L)
+  #  b <- textcnt(document,method="string",n=1L)
+  #  c <- intersect(names(a), names(b))
+  #  feature[i] <- as.numeric(length(a)==length(c))}
+  feature
 }
-stringprint("test")
+
+test<-function(){
+  summary(all.queryterms(queries$search_term, queries$product_title))
+}
+
+
+
+
+
+
+ReadInfunc<- function(){
+  descriptions<<-read.csv(file="product_descriptions.csv", row.names = 1, stringsAsFactors = FALSE)
+  queries<<-read.csv(file="query_product.csv", row.names = 1, stringsAsFactors = FALSE)
+}
+
+checkFunc<-function(){
+  !exists("queries")||!exists("descriptions")
+}
