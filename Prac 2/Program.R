4bc98ae3e19a30248fd05fc01d7dad1ba57648f8
Lunariz <lukas_donkers@hotmail.com>
2016-06-10 13:00:03 +0200

    readqueryproduct

--- Prac 2/Program.R
+++ Prac 2/Program.R
@@ -6,7 +6,13 @@ library("nnet")
 
 Main<- function(){
   #start het programma
-  
+  readQueryProduct()
+}
+
+readQueryProduct <- function() {
+  query_product.dat <- read.csv("query_product.csv", stringsAsFactors=FALSE)
+  str(query_product.dat)
+  query_product.dat[query_product.dat[,1] == 9]
 }
 
 stringprint <-function(x){
